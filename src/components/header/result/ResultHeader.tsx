6528f83a3e0284ef55883722b16244d640caf211
Torben Reetz <torben.reetz@mindmatters.de>
2025-10-05 22:16:07 +0200

    add icons

--- src/components/header/result/ResultHeader.tsx
+++ src/components/header/result/ResultHeader.tsx
@@ -1,23 +1,16 @@
 import {HeaderContainer} from "../HeaderContainer";
-import TickIcon from "../../../assets/icons/tick.svg?react"
-import CrossIcon from "../../../assets/icons/cross.svg?react"
 import styles from "./ResultHeader.module.css"
-import {cns} from "../../../utils/cns";
 
 export function CorrectResultHeader() {
-    return <HeaderContainer>
-        <div className={cns(styles.iconContainer, styles.correct)}>
-            <TickIcon/>
-        </div>
-        <strong>correct</strong>
+    return <HeaderContainer className={styles.correct} >
+        <div className={styles.icon}>check</div>
+        <strong >correct</strong>
     </HeaderContainer>
 }
 
 export function IncorrectResultHeader() {
-    return <HeaderContainer>
-        <div className={cns(styles.iconContainer, styles.incorrect)}>
-            <CrossIcon/>
-        </div>
+    return <HeaderContainer className={styles.incorrect}>
+        <div className={styles.icon}>close</div>
         <strong>wrong</strong>
     </HeaderContainer>
 }
