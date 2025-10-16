c361bdd87d2a7dab7ea86d33470f62bc4665910d
Torben Reetz <torben.reetz@mindmatters.de>
2025-10-16 23:03:29 +0200

    use cache in sw

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,8 @@
+const CACHE_RUNTIME = 'faerbl-v1'
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('faerbl-v1').then((cache) => {
+    caches.open(CACHE_RUNTIME).then((cache) => {
       return cache.addAll([
         '/faerbl/',
         '/faerbl/index.html'
@@ -15,7 +17,25 @@ self.addEventListener('activate', () => {
 });
 
 // Empty fetch event handler
-self.addEventListener('fetch', () => {
-  // This is an empty fetch event handler
-  // It will be triggered for all fetch requests but won't do anything
+self.addEventListener('fetch', (event) => {
+
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(CACHE_RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+
 });
